b5d0885e541c7a4a1d436255bb3ddbbaf9d76f27
Robson Costa <rcostainfo@gmail.com>
2020-04-29 12:23:14 +0100

    Projeto testado e finalizado

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xadrez-console
 Aplicativo de jogo de xadrez (Console app)
+
+App desenvolvida no curso da plataforma Udemy.
+"C# COMPLETO 2020 Programação Orientada a Objetos + Projetos"
+https://www.udemy.com/course/programacao-orientada-a-objetos-csharp/
---
+++ xadrez-console/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch(TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+             
+            Console.ReadLine();
+        }
+    }
+}
